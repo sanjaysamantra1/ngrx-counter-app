08aa04c29bb91bf7097b1eb3cfde07c867e4cf36
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-06-27 08:32:19 +0530

    added actions

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { decrement, increment, reset } from '../../counter-ngrx/counter.actions';
 
 @Component({
   selector: 'app-counter',
@@ -16,5 +17,14 @@ export class CounterComponent {
   constructor(private store: Store<{ count: number }>) {
     this.count$ = store.select('count');
     console.log(this.count$)
+  }
+  increment() {
+    this.store.dispatch(increment())
+  }
+  decrement() {
+    this.store.dispatch(decrement())
+  }
+  reset() {
+    this.store.dispatch(reset())
   }
 }
